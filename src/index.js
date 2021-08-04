3b2ec3199ac6ba9dc0275348eb36933ab3e14750
Yauheni Shatau <jack13only@gmail.com>
2021-08-04 10:43:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0
+  let minNumber = array[0]
+  for(let i = 0; i < array.length; i++) {
+    if (array[i] < minNumber) minNumber =  array[i]
+  }
+  return minNumber
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0
+  let maxNumber = array[0]
+  for(let i = 0; i < array.length; i++) {
+    if (array[i] > maxNumber) maxNumber =  array[i]
+  }
+  return maxNumber
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0
+  return array.reduce((sum, current) => sum + current, 0)/array.length
 }
